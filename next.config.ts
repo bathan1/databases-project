85500c1e593f825cbd91eb8100442d4ec097efed
bathan1 <side_nathan@hotmail.com>
2026-05-05 22:52:02 -0400

    Get static build working

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+const nextConfig: import('next').NextConfig = {
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true
+  }
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+export default nextConfig
